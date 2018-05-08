11484b5bdc55545f5e9f723d7f89929ae96b51c4
Michał Brusiło <mibru@interia.pl>
2018-05-08 06:45:25 +0200

    Add program.js

---
+++ program.js
@@ -0,0 +1,30 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+  
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+
+        switch (instruction) {
+        	case '/exit':
+            	process.stdout.write('Quitting app!\n');
+            	process.exit();    
+            	break;
+
+           	case '/nodev':
+			process.stdout.write('version of node' + process.versions.node);
+            process.exit(); 
+            break;
+
+        	case '/lang':
+			process.stdout.write('language ' + process.env.LANG);
+            process.exit(); 
+            break;
+
+            default:
+            process.stderr.write('Wrong instruction!')
+        }
+      
+    }
+});
